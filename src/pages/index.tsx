0a5af8416eabefb99ae901a54ee166a709ef80a3
Vindexus <vindexus@gmail.com>
2023-06-14 06:51:33 -0700

    Added blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { HeadFC, PageProps } from "gatsby"
 import {Link} from "gatsby";
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from "../components/seo";
 
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -12,7 +13,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        src={"../images/marines.png"}
       />
     </Layout>
   )
@@ -20,4 +21,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <Seo title={"Home Page"} />
